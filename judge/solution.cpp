5d0eed4459a7e00bab776d3a8fecdcf1fb13f82f
MgDyqaq <mingdynasty@love.ml>
2026-09-08 21:35:59 +0800

    Update solution.cpp at 2026-09-08T13:35:59.584Z

--- judge/solution.cpp
+++ judge/solution.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-using namespace std;
-long long a,b;
-int main()
-{
-    cin>>a>>b;
-    cout<<a+b;
-    return 0;
-}
+        using namespace std;
+        
+        int main() {
+            int a, b;
+            cin >> a >> b;
+            cout << a + b << endl;
+            return 0;
+        }
